17d05ff0f7eb8dae96ac4c9117cc777417f6d967
MaliaWilken4 <mawilken4@gmail.com>
2025-11-12 04:04:16 -0800

    Forgot to push InventoryPanel.cs script as well

--- Assets/Scripts/InventoryPanel.cs
+++ Assets/Scripts/InventoryPanel.cs
@@ -8,9 +8,37 @@ public class InventoryPanel : MonoBehaviour
     public GameObject EquipmentTab;
     public GameObject AbilitiesTab;
 
+    public PauseMenuManager isPaused;
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPaused && Input.GetKeyDown(KeyCode.Alpha1))
+            switchFoodTab();
+        else if (isPaused && Input.GetKeyDown(KeyCode.Alpha2))
+            switchEquipmentTab();
+        else if (isPaused && Input.GetKeyDown(KeyCode.Alpha3))
+            switchAbilitiesTab();
+    }
+
+    public void switchFoodTab()
+    {
+        FoodTab.SetActive(true);
+        EquipmentTab.SetActive(false);
+        AbilitiesTab.SetActive(false);
+    }
+
+    public void switchEquipmentTab()
+    {
+        EquipmentTab.SetActive(true);
+        AbilitiesTab.SetActive(false);
+        FoodTab.SetActive(false);
+    }
+
+    public void switchAbilitiesTab()
+    {
+        AbilitiesTab.SetActive(true);
+        FoodTab.SetActive(false);
+        EquipmentTab.SetActive(false);
     }
 }
